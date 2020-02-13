0d0225c276e6b2e81cfbc09d5c5a8b56b2821beb
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-13 17:56:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  a = []
   if name_hash == {}
     return nil
   else
-    name_hash.collect do |key, value|
+    name_hash.each do |key, value|
+    a.push(value)
+  end
+  
       
       
 end
