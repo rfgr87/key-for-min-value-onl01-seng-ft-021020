f31b3d531a02d5dd4a8cca12d976f054343d290d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-13 17:41:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  a = []
-  name_hash.each do |key, value|
+  if name_hash == {}
+    return nil
+  else
+    name_hash = name_hash.sort {|k,v| 
     
 
+end
+
+def min_key_value_pair (test)
+  if test == {}
+       return nil
+  else
+       test = test.sort_by {|k, v| v}
+       test[0]
+   end
 end
